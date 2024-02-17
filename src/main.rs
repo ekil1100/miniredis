26bab4d1bc9e7f394149a0d43100c4c433970240
Like <like@ekil.sh>
2024-02-17 19:24:21 +0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use mini_redis::Connection;
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main(){
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
+
+}
+
+async fn process(socket: TcpStream){
+    let mut connection = Connection::new(socket);
 }
